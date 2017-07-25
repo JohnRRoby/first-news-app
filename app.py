50910b71c9d308901c236fddc8875c13bd86324e
John R. Roby <johnroby@searchlightnm.com>
2017-07-24 20:32:54 -0400

    Created basic table

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+import csv
 from flask import Flask
 from flask import render_template # this function combines data w HTML to make webpage
 app = Flask(__name__)
+
+def get_csv():
+	csv_path = "./static/la-riots-deaths.csv"
+	csv_file = open(csv_path, "rb")
+	csv_obj = csv.DictReader(csv_file)
+	csv_list = list(csv_obj)
+	return(csv_list)
 
 @app.route("/") # flask decorator connecting index function below with root URL, /
 
 def index(): # function returns the rendered index.html template
 	template = "index.html"
-	return render_template(template)
+	object_list = get_csv()
+	return render_template(template, object_list = object_list)
+	
 
 if __name__ == '__main__':
 	# launch flask test server
