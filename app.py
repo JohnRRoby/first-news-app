712aab3cab793b52abb19216f306a1f3deb8370a
John R. Roby <johnroby@searchlightnm.com>
2017-07-24 21:03:00 -0400

    Created detail page for each victim

--- app.py
+++ app.py
@@ -11,12 +11,22 @@ def get_csv():
 	return(csv_list)
 
 @app.route("/") # flask decorator connecting index function below with root URL, /
-
 def index(): # function returns the rendered index.html template
 	template = "index.html"
 	object_list = get_csv()
 	return render_template(template, object_list = object_list)
 	
+@app.route("/<row_id>/")	# see how that works, contrast w one before index function.
+def detail(row_id):
+		template = "detail.html"
+		object_list = get_csv()
+		for row in object_list:
+			if row["id"] == row_id:
+				return render_template(template, object = row)
+# above note both url route and function take an argument, row_id.
+# goal is for number passed to URL to go into the function where it will be used to 
+# pull the corresponding ID from the csv. then we'll pass that to the template to render the unique page.
+
 
 if __name__ == '__main__':
 	# launch flask test server
